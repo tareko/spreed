bf75ee6ffb3f50ffe7525428323c484f57f97d50
Maksim Sukharev <antreesy.web@gmail.com>
2026-04-21 18:25:17 +0200

    wip: introduce floatingCall initialization logic
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/floatingCall.ts
+++ src/floatingCall.ts
@@ -3,6 +3,17 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import type { Conversation } from './types/index.ts'
+
+import { createApp, defineAsyncComponent, reactive } from 'vue'
+import { CONVERSATION } from './constants.ts'
+import { ALLOWED_DIRECT_CALL_URL_REGEX } from './floatingCallTrigger.ts'
+import { createMemoryRouter } from './router/router.ts'
+import { createConversation, fetchConversation } from './services/conversationsService.ts'
+import store from './store/index.js'
+import pinia from './stores/pinia.ts'
+import { NextcloudGlobalsVuePlugin } from './utils/NextcloudGlobalsVuePlugin.js'
+
 /**
  * Initializes the floating container on the page
  *
@@ -11,5 +22,56 @@
  *            (<...>/apps/call/<token>#direct-call)
  */
 export async function handleStartFloatingCall(url: URL) {
-	// TODO
+	try {
+		const userId = url.searchParams.get('callUser')
+		let conversation: Conversation
+
+		if (userId) {
+			conversation = (await createConversation({
+				roomType: CONVERSATION.TYPE.ONE_TO_ONE,
+				participants: { users: [userId] },
+			})).data.ocs.data
+		} else {
+			// Already a conversation token
+			const matchGroup = ALLOWED_DIRECT_CALL_URL_REGEX.exec(url.toString())![2]
+			conversation = (await fetchConversation(matchGroup)).data.ocs.data
+			if (conversation.type !== CONVERSATION.TYPE.ONE_TO_ONE) {
+				// Ignore group/public/other conversations and proceed with normal navigation
+				window.location.assign(url.href)
+				return
+			}
+		}
+
+		store.dispatch('addConversation', conversation)
+		const token = conversation.token
+
+		if (!window.OCA.Talk) {
+			window.OCA.Talk = reactive({})
+		}
+
+		const floatingCallContainer = document.createElement('div')
+		floatingCallContainer.id = `talk-floating-call-${token}`
+		document.body.appendChild(floatingCallContainer)
+		const router = createMemoryRouter()
+
+		const FloatingCallOverlay = defineAsyncComponent(() => import('./FloatingCallOverlay.vue'))
+		const instance = createApp(FloatingCallOverlay, {
+			token,
+		})
+			.use(pinia)
+			.use(store)
+			.use(router)
+			.use(NextcloudGlobalsVuePlugin)
+		window.OCA.Talk.instance = instance
+		window.OCA.Talk.unmountInstance = function() {
+			instance.unmount()
+			document.body.removeChild(floatingCallContainer)
+			delete window.OCA.Talk.instance
+			delete window.OCA.Talk.unmountInstance
+		}
+
+		instance.mount(document.getElementById(`talk-floating-call-${token}`)!)
+	} catch (error) {
+		console.error(error)
+	}
 }
